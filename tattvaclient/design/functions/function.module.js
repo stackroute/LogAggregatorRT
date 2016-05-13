f7ae581305c73ee48e1087e4408f6e2abcd25c14
prasad4fun <singh_suryaprakash@yahoo.com>
2016-05-13 20:39:34 +0530

    Removed duplicate funtion folder and corrected them to be inside design

--- tattvaclient/design/functions/function.module.js
+++ tattvaclient/design/functions/function.module.js
@@ -1,19 +1,40 @@
-angular.module("tattva").config(['$stateProvider','$urlRouterProvider', function($stateProvider){
+angular.module('tattva').config(['$stateProvider','$urlRouterProvider', function($stateProvider){
   $stateProvider
-  .state('design',{
-    url: "/design",
-    views: {
-      "header" : {
-        templateUrl: "/home/template/header.html",
-        controller: "HeaderCtrl"
-      },
-      "content@" : {
-        templateUrl: "/partials/design.html",
-        controller: "orgCtrl"
-      },
-      "footer" : {
-        templateUrl: "/home/template/footer.html"
-      }
-    }
+  // .state('design',{
+  //   url: "/design",
+  //   views: {
+  //     "header" : {
+  //       templateUrl: "/home/template/header.html",
+  //       controller: "HeaderCtrl"
+  //     },
+  //     "content@" : {
+  //       templateUrl: "/partials/design.html",
+  //       controller: "orgCtrl"
+  //     },
+  //     "footer" : {
+  //       templateUrl: "/home/template/footer.html"
+  //     }
+  //   }
+  //
+  // })
 
-  })}]);
+  .state('design.function',
+  {
+    url:'/function',
+    templateUrl: "/Function/template/functionlist.html",
+    controller:"functionlistCtrl"
+
+  })
+  .state('design.functionEdit', {
+    url: '/functional/:functionname',
+    templateUrl: '/Function/template/cfunctions.html',
+    controller: 'functionEditCtrl'
+  })
+  .state('design.addfunction', {
+    url:"/addFunction",
+    templateUrl:"/Function/template/cfunctions.html"
+  })
+
+
+}
+]);
