09b1ed32e0f36f1387d85ba9c1b300ec122e40d7
prasad4fun <singh_suryaprakash@yahoo.com>
2016-05-13 21:17:15 +0530

    fixed function template path issue, all design subcomponents working except namespace

--- tattvaclient/design/functions/function.module.js
+++ tattvaclient/design/functions/function.module.js
@@ -1,40 +1,20 @@
-angular.module('tattva').config(['$stateProvider','$urlRouterProvider', function($stateProvider){
+angular.module('tattva')
+.config(['$stateProvider','$urlRouterProvider',
+function($stateProvider){
   $stateProvider
-  // .state('design',{
-  //   url: "/design",
-  //   views: {
-  //     "header" : {
-  //       templateUrl: "/home/template/header.html",
-  //       controller: "HeaderCtrl"
-  //     },
-  //     "content@" : {
-  //       templateUrl: "/partials/design.html",
-  //       controller: "orgCtrl"
-  //     },
-  //     "footer" : {
-  //       templateUrl: "/home/template/footer.html"
-  //     }
-  //   }
-  //
-  // })
-
   .state('design.function',
   {
     url:'/function',
-    templateUrl: "/Function/template/functionlist.html",
+    templateUrl: "/design/functions/template/functionlist.html",
     controller:"functionlistCtrl"
-
   })
   .state('design.functionEdit', {
     url: '/functional/:functionname',
-    templateUrl: '/Function/template/cfunctions.html',
+    templateUrl: '/design/functions/template/cfunctions.html',
     controller: 'functionEditCtrl'
   })
   .state('design.addfunction', {
     url:"/addFunction",
-    templateUrl:"/Function/template/cfunctions.html"
+    templateUrl:"/design/functions/template/cfunctions.html"
   })
-
-
-}
-]);
+}]);
