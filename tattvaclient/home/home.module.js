82775fc6665f0f7cd7ef3211098b90b68a5f5170
jasjeetkaur <jasjeet.gehal@wipro.com>
2016-05-16 14:25:22 +0000

    fixing header font-color

--- tattvaclient/home/home.module.js
+++ tattvaclient/home/home.module.js
@@ -1,26 +1,28 @@
 angular.module('tattva')
 .config(["$mdThemingProvider", function($mdThemingProvider) {
-  var cyan = $mdThemingProvider.extendPalette('cyan', {
-    '500' : '00BCD4'
-    // '500' : '00BCD4',
-    // '50'  : 'E0F7FA',
-    // '100' : 'B2EBF2',
-    // '200' : '80DEEA',
-    // '300' : '4DD0E1',
-    // '400' : '26C6DA',
-    // '500' : '00BCD4',
-    // '600' : '00ACC1',
-    // '700' : '0097A7',
-    // '800' : '00838F',
-    // '900' : '006064'
+  $mdThemingProvider.definePalette('cyanTheme',{
+    '500' : '00BCD4',
+    '50'  : 'E0F7FA',
+    '100' : 'B2EBF2',
+    '200' : '80DEEA',
+    '300' : '4DD0E1',
+    '400' : '26C6DA',
+    '500' : '00BCD4',
+    '600' : '00ACC1',
+    '700' : '0097A7',
+    '800' : '00838F',
+    '900' : '006064',
+    'A100': '84FFFF',
+    'A200': '18FFFF',
+    'A400': '00E5FF',
+    'A700': '00B8D4',
+    'contrastDefaultColor': 'light',
+    'contrastDarkColors': ['50', '100','200', '300', '400', 'A100'] //hues which contrast should be 'dark' by default
   });
 
-  $mdThemingProvider.definePalette('primary', cyan);
-
   $mdThemingProvider.theme('default')
-  .primaryPalette('primary')
-  .accentPalette('light-blue')
-  .warnPalette('green');
+  .primaryPalette('cyanTheme') //500
+  .accentPalette('cyanTheme') //600
 }]);
 
 angular.module('tattva')
