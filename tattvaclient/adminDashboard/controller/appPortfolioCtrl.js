db47fa3f9e2345fa7127a0c3be974cdf21cbca47
baba3375 <baba3375>
2016-07-15 13:16:19 +0000

    orgDialog

--- tattvaclient/adminDashboard/controller/appPortfolioCtrl.js
+++ tattvaclient/adminDashboard/controller/appPortfolioCtrl.js
@@ -1,24 +1,26 @@
 angular.module('tattva')
 .controller("appPortfolioCtrl",['$scope','$http','$state','$mdDialog','adminFactory',
- function($scope, $http, $state,$mdDialog,adminFactory) {
+function($scope, $http, $state,$mdDialog,adminFactory) {
 
   adminFactory.orgList().then(function(res){
     $scope.orgs=res.data;
-      console.log('$scope.orgs',$scope.orgs);
+    console.log('$scope.orgs',$scope.orgs);
   },function(res){
     console.log("error",res.data.error);
   })
 
   $scope.orgClick = function(ev,orgSite){
-      $mdDialog.show({
-        controller: "orgDialogCtrl",
-        templateUrl:'adminDashboard/template/orgDialog.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        locals:{orgSite:orgSite},
-        escapeToClose:true,
-        fullscreen: true
-      });
+    $mdDialog.show({
+      controller: "orgDialogCtrl",
+      templateUrl:'adminDashboard/template/orgDialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      locals:{orgSite:orgSite},
+      escapeToClose:true,
+      fullscreen: true
+    });
   };
+
+
 
 }]);
