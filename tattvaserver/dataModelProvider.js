bece5fae759d46a4f8188a65d73aad2f04ba44ed
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2016-07-15 15:32:53 +0530

    Removing rogue files

--- tattvaserver/dataModelProvider.js
+++
@@ -1,9 +0,0 @@
-var namespacesSchema = require("../namespace/namespace.js");
-var organisationsSchema = require("../organisation/organisations.js");
-var watchListSchema = require("../watchlists/watchlists.js");
-var watchLoopSchema = require("../watchloop/watchloop.js");
-
-var dataModelProvider = function(SchemaName,OrgSite){
-  var modelObj = mongoose.model('modelObj',SchemaName);
-  
-}
