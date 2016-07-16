ab04f6d3a0e715b225a220a147a2146eb735fd7e
PrasannaKainkaryam2 <PrasannaKainkaryam2>
2016-07-16 06:40:26 +0000

    Fixing the directory path for config and log folder

--- applogger.js
+++ applogger.js
@@ -1,7 +1,9 @@
 var log4js = require('log4js');
+var path = require('path');
 
-log4js.configure('./log4js.conf.json', {
-    cwd: './logs/'
+//Log4js is taking relative path of the file, hence better to fix the path in the config
+log4js.configure(path.join(__dirname, 'log4js.conf.json'), {
+    cwd: path.join(__dirname, 'logs/')
 });
 
 var logger = log4js.getLogger();
