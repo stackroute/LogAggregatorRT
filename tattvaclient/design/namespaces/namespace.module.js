d686ac747c793c7331c4d9f9f0b77def4b2df5e4
vagrant <sridivya.morukurthi@wipro.com>
2016-08-30 12:02:48 +0000

    namespace page

--- tattvaclient/design/namespaces/namespace.module.js
+++ tattvaclient/design/namespaces/namespace.module.js
@@ -1,27 +1,28 @@
-angular.module('tattva')
-.config(['$stateProvider','$urlRouterProvider',
-function($stateProvider){
-  $stateProvider
-  .state('design.namespace', {
-    url: "/namespaces",
-    templateUrl: "/design/namespaces/template/listNamespace.html",
-    controller:"listNamespaceCtrl",
-    resolve:{
-      nameSpaceColln:function(namespaceFactory) {
-        return namespaceFactory.getNameSpace().then(function(response){
-          return response;
-        }, function(response){
-          console.log("error in getting namespace data: ", response);
-        });
-      }
-    }
-  }).state('design.createNamespace', {
-    url: "/namespace/new",
-    templateUrl: "/design/namespaces/template/createNamespace.html",
-    controller:"createNamespaceCtrl"
-  }).state('design.editNamespace', {
-    url: "/namespace/:editNamespaceData",
-    templateUrl: "/design/namespaces/template/createNamespace.html",
-    controller:"createNamespaceCtrl"
-  })
-}]);
+    angular.module('tattva')
+        .config(['$stateProvider', '$urlRouterProvider',
+            function($stateProvider) {
+                $stateProvider
+                    .state('design.namespace', {
+                        url: "/namespaces",
+                        templateUrl: "/design/namespaces/template/NamespaceList.html",
+                        controller: "listNamespaceCtrl",
+                        resolve: {
+                            nameSpaceColln: function(namespaceFactory) {
+                                return namespaceFactory.getNameSpace().then(function(response) {
+                                    return response;
+                                }, function(response) {
+                                    console.log("error in getting namespace data: ", response);
+                                });
+                            }
+                        }
+                    }).state('design.createNamespace', {
+                        url: "/namespace/new",
+                        templateUrl: "/design/namespaces/template/createNamespace.html",
+                        controller: "createNamespaceCtrl"
+                    }).state('design.editNamespace', {
+                        url: "/namespace/:editNamespaceData",
+                        templateUrl: "/design/namespaces/template/createNamespace.html",
+                        controller: "createNamespaceCtrl"
+                    })
+            }
+        ]);
