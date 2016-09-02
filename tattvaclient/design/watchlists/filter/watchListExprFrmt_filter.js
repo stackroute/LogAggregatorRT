db9d7f7a204aa715b274597e6e24ce8cccf15988
Bhavani <bhavanibhanu24@gmail.com>
2016-09-02 04:13:42 +0000

    minor watchlist refactoring | made watchlist expression as directive

--- tattvaclient/design/watchlists/filter/watchListExprFrmt_filter.js
+++ tattvaclient/design/watchlists/filter/watchListExprFrmt_filter.js
@@ -1,24 +1,23 @@
 //customfilter for watch expression format
 angular.module("tattva")
-    .filter('watchExprFormat', function() {
+    .filter('watchListExpr', function() {
         return function(wlstdef) {
             var exprtext = "";
             if (wlstdef !== undefined && wlstdef.expressions.length >= 1) {
                 wlstdef.expressions.forEach(function(exprData) {
                     if (exprData.watch.lfield.exprAsText) {
-                        console.log(exprData.watch.lfield.exprAsText);
                         exprtext += exprData.watch.lfield.exprAsText;
                     }
 
                     if (exprData.watch.operator) {
-                        exprtext += exprData.watch.operator;
+                        exprtext += "  "+exprData.watch.operator;
                     }
 
                     if (exprData.watch.rfield.exprAsText) {
-                        exprtext += exprData.watch.rfield.exprAsText
+                        exprtext += "  "+exprData.watch.rfield.exprAsText
                     }
                     if(exprtext !== "" && exprData.child !== ""){
-                        exprtext += " AND ";
+                        exprtext += "  AND  ";
                     }
                 });
             }
