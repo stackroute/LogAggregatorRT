6fc688119aa5daacc1a0bf13723cc08c8ee3110c
kritikasingla17 <singlakritika92@gmail.com>
2016-09-14 07:03:22 +0000

    composite function

--- tattvaserver/compositefunction/compositefunction.js
+++ tattvaserver/compositefunction/compositefunction.js
@@ -1,41 +1,4 @@
 var functionProvider = require("../datafunctionlib/datafnprovider");
-
-/*
-var execute = function (fkn_name, paramObj) {
-  var response = getCompositFunctionDefn(fkn_name);
-  var result=[];
-  for(var i=0,j=0;i<3;i++) {
-    // console.log(response[0].expression[i].type);
-    if(response[0].expression[i].type=="function") {
-
-      var fnParamData=[];
-      fnParamData.push(paramObj[response[0].expression[i].varmap[0].targetvar]);
-      fnParamData.push(paramObj[response[0].expression[i].varmap[1].targetvar]);
-      var functionModule = new functionProvider(response[0].expression[i].name);
-      result[j] = functionModule.evaluate(fnParamData);
-      j++;
-    }
-  }
-  var final;
-  if(response[0].expression[1].name.toUpperCase()=="ADD +")
-  {
-    final= result[0].output+result[1].output;
-  }
-  if(response[0].expression[1].name.toUpperCase()=="SUB -")
-  {
-    final= result[0].output-result[1].output;
-  }
-  if(response[0].expression[1].name.toUpperCase()=="MUL *")
-  {
-    final= result[0].output*result[1].output;
-  }
-  if(response[0].expression[1].name.toUpperCase()=="DIV /")
-  {
-    final= result[0].output/result[1].output;
-  }  
-  return final;
-}*/
-
 function getCompositFunctionDefn() {
   return final= {
     "name": "qwert",
