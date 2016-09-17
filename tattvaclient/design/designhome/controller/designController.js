2bdd732e37fddbb3804e88a6fcb6d3a4e505c0df
khatwachandan5 <khatwachandan5@gmail.com>
2016-09-17 12:37:10 +0530

    index handling of tabular content on state change

--- tattvaclient/design/designhome/controller/designController.js
+++ tattvaclient/design/designhome/controller/designController.js
@@ -1,29 +1,28 @@
 angular.module('tattva')
-.controller('designController',['$state','$scope', function($state,$scope){
-
-	if($state.current.name == 'design.summary'){
-		$scope.selectedIndex = 0;
-	}
-	else if($state.current.name == 'design.namespace'){
-		$scope.selectedIndex = 1;
-	}
-	else if($state.current.name == 'design.instance'){
-		$scope.selectedIndex = 2;
-	}
-	else if($state.current.name == 'design.streams'){
-		$scope.selectedIndex = 3;
-	}
-	else if($state.current.name == 'design.historicQuery'){
-		$scope.selectedIndex = 4;
-	}
-	else if($state.current.name == 'design.function'){
-		$scope.selectedIndex = 5;
-	}
-	else if($state.current.name == 'design.constant'){
-		$scope.selectedIndex = 6;
-	}
-	else if($state.current.name == 'design.watchlist'){
-		$scope.selectedIndex = 7;
-	}
-
-}]);
+    .controller('designController', ['$state', '$scope', '$rootScope', function($state, $scope, $rootScope) {
+        function checkState(data) {
+            if (data.sname == 'design.summary') {
+                $scope.selectedIndex = 0;
+            } else if (data.sname == 'design.namespace') {
+                $scope.selectedIndex = 1;
+            } else if (data.sname == 'design.instance') {
+                $scope.selectedIndex = 2;
+            } else if (data.sname == 'design.streams') {
+                $scope.selectedIndex = 3;
+            } else if (data.sname == 'design.historicQuery') {
+                $scope.selectedIndex = 4;
+            } else if (data.sname == 'design.function') {
+                $scope.selectedIndex = 5;
+            } else if (data.sname == 'design.constant') {
+                $scope.selectedIndex = 6;
+            } else if (data.sname == 'design.watchlist') {
+                $scope.selectedIndex = 7;
+            }
+        }
+        $rootScope.$on("index", function(event, data) {
+            console.log(data);
+            checkState(data);
+        });
+        console.log($state.current.name);
+        checkState({ sname: $state.current.name })
+    }]);
