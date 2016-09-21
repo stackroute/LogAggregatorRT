6c5994eda818eca407020a2c37874f0531c7acf5
kritikasingla17 <singlakritika92@gmail.com>
2016-09-21 13:21:03 +0000

    reverting the merge conflicts in compositefunction

--- tattvaserver/datafunctionlib/compositeFunction/compositeFunction.js
+++ tattvaserver/datafunctionlib/compositeFunction/compositeFunction.js
@@ -1,121 +1,83 @@
 module.exports = function() {
-        var compositeFunction_router = require("../../compositefunction/compositefunction_routes");
-        return {
-            execute: function(functionObject, paramObj) {
-                    var functionProvider = require("../datafnprovider");
-                    var result = [];
-                    var i = 0;
+
+    var compositeFunction_router = require("../../compositefunction/compositefunction_routes");
+
+    return {
+        execute: function(functionObject, paramObj) {
+
+            var functionProvider = require("../datafnprovider");
+            var result = [];
+            var i = 0;
 
 
-                    if (functionObject != undefined) {
+            if (functionObject != undefined) {
 
-                        for (expr in functionObject.expression) {
+                for (expr in functionObject.expression) {
 
-                            if (expr < functionObject.expression.length) {
-                                var fnParamData = [];
+                    if (expr < functionObject.expression.length) {
+                        var fnParamData = [];
 
-                                fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].lhs.varmap[0].targetvar]));
-                                fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].lhs.varmap[1].targetvar]));
+                        fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].lhs.varmap[0].targetvar]));
+                        fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].lhs.varmap[1].targetvar]));
 
 
-                                var functionModule = new functionProvider(functionObject.expression[expr].lhs.name);
-                                var lhs = functionModule.evaluate(fnParamData);
-                                console.log("lhs" + lhs.output);
-                                var fnParamData = [];
-                                fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].rhs.varmap[0].targetvar]));
-                                fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].rhs.varmap[1].targetvar]));
-                                var functionModule = new functionProvider(functionObject.expression[expr].rhs.name);
-                                var rhs = functionModule.evaluate(fnParamData);
-                                console.log("rhs" + rhs.output);
-                                if (functionObject.expression[expr].operator.name == "+") {
-                                    console.log("in add");
-                                    result[i] = lhs.output + rhs.output;
-                                }
-                                if (functionObject.expression[expr].operator.name == "-") {
-                                    console.log("in sub");
-                                    result[i] = lhs.output - rhs.output;
-                                }
-                                if (functionObject.expression[expr].operator.name == "*") {
-                                    result[i] = lhs.output * rhs.output;
-                                }
-                                if (functionObject.expression[expr].operator.name == "/") {
-                                    result[i] = lhs.output / rhs.output;
-                                }
-                                i++;
-                                result[i] = functionObject.expression[expr].join_By.name;
-                                i++;
+                        var functionModule = new functionProvider(functionObject.expression[expr].lhs.name);
+                        var lhs = functionModule.evaluate(fnParamData);
 
-                                var functionProvider = require("../datafnprovider");
-                                var result = [];
-                                var i = 0;
-                                if (functionObject != undefined) {
-                                    for (expr in functionObject.expression) {
-                                        if (expr < functionObject.expression.length) {
-                                            var fnParamData = [];
-                                            fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].lhs.varmap[0].targetvar]));
-                                            fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].lhs.varmap[1].targetvar]));
-                                            var functionModule = new functionProvider(functionObject.expression[expr].lhs.name);
-                                            var lhs = functionModule.evaluate(fnParamData);
-                                            console.log("lhs" + lhs.output);
-                                            var fnParamData = [];
-                                            fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].rhs.varmap[0].targetvar]));
-                                            fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].rhs.varmap[1].targetvar]));
-                                            var functionModule = new functionProvider(functionObject.expression[expr].rhs.name);
-                                            var rhs = functionModule.evaluate(fnParamData);
-                                            console.log("rhs" + rhs.output);
-                                            if (functionObject.expression[expr].operator.name == "ADD +") {
-                                                console.log("in add");
-                                                result[i] = lhs.output + rhs.output;
-                                            }
-                                            if (functionObject.expression[expr].operator.name == "SUB -") {
-                                                console.log("in sub");
-                                                result[i] = lhs.output - rhs.output;
-                                            }
-                                            if (functionObject.expression[expr].operator.name == "MUL *") {
-                                                result[i] = lhs.output * rhs.output;
-                                            }
-                                            if (functionObject.expression[expr].operator.name == "DIV /") {
-                                                result[i] = lhs.output / rhs.output;
-                                            }
-                                            i++;
-                                            result[i] = functionObject.expression[expr].join_By.name;
-                                            i++;
-                                        }
-                                    }
+                        var fnParamData = [];
+                        fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].rhs.varmap[0].targetvar]));
+                        fnParamData.push(parseFloat(paramObj[functionObject.expression[expr].rhs.varmap[1].targetvar]));
+                        var functionModule = new functionProvider(functionObject.expression[expr].rhs.name);
+                        var rhs = functionModule.evaluate(fnParamData);
 
-                                }
-                                var final = {};
-                                final.error = false;
-                                if (result.length > 2) {
-                                    for (var j = 1; j < result.length; j = j + 2) {
-                                        if (result[j] == "ADD +") {
+                        if (functionObject.expression[expr].operator.name == "+") {
 
-                                            result[j + 1] = result[j - 1] + result[j + 1];
-                                            final.output = result[j + 1];
-                                        }
-                                        if (result[j] == "SUB -") {
-                                            console.log("in sub---");
-                                            console.log(result[j - 1]);
-                                            console.log(result[j + 1]);
-                                            result[j + 1] = result[j - 1] - result[j + 1];
-                                            console.log(result[j + 1]);
-                                            final.output = result[j + 1];
-                                            console.log(final.output);
-                                        }
-                                        if (result[j] == "MUL *") {
-                                            result[j + 1] = result[j - 1] * result[j + 1];
-                                            final.output = result[j + 1];
-                                        }
-                                        if (result[j] == "DIV /") {
-                                            result[j + 1] = result[j - 1] / result[j + 1];
-                                            final.output = result[j + 1];
-                                        }
-                                    }
-                                } else {
-                                    final.output = result[0];
-                                }
-                                return final;
-
-                            }
+                            result[i] = lhs.output + rhs.output;
                         }
+                        if (functionObject.expression[expr].operator.name == "-") {
+
+                            result[i] = lhs.output - rhs.output;
+                        }
+                        if (functionObject.expression[expr].operator.name == "*") {
+                            result[i] = lhs.output * rhs.output;
+                        }
+                        if (functionObject.expression[expr].operator.name == "/") {
+                            result[i] = lhs.output / rhs.output;
+                        }
+                        i++;
+                        result[i] = functionObject.expression[expr].join_By.name;
+                        i++;
                     }
+                }
+            }
+            var final = {};
+            final.error = false;
+            if (result.length > 2) {
+                for (var j = 1; j < result.length; j = j + 2) {
+                    if (result[j] == "+") {
+                        result[j + 1] = result[j - 1] + result[j + 1];
+                        final.output = result[j + 1];
+                    }
+                    if (result[j] == "-") {
+                        result[j + 1] = result[j - 1] - result[j + 1];
+                        final.output = result[j + 1];
+                    }
+                    if (result[j] == "*") {
+                        result[j + 1] = result[j - 1] * result[j + 1];
+                        final.output = result[j + 1];
+                    }
+                    if (result[j] == "/") {
+                        result[j + 1] = result[j - 1] / result[j + 1];
+                        final.output = result[j + 1];
+                    }
+                }
+            } else {
+                final.output = result[0];
+            }
+
+
+            return final;
+
+        }
+    }
+}
