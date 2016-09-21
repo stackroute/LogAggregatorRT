d703d96e467425c60a1c5a6dbc5928d2b6f364b4
kritikasingla17 <singlakritika92@gmail.com>
2016-09-21 05:25:50 +0000

    view functionality on composite function

--- tattvaclient/design/functions/factory/functionfactory.js
+++ tattvaclient/design/functions/factory/functionfactory.js
@@ -1,67 +1,67 @@
 angular.module('tattva')
-.factory('functionFactory', ['$http','$q', function($http, $q){
-  var factory = {
+    .factory('functionFactory', ['$http', '$q', function($http, $q) {
+        var factory = {
 
-    saveFunction: function(createFunction) {
-      return $q(function(resolve, reject) {
-        $http.post('/compositefunction/'+createFunction.name,createFunction)
-        .then(function(res) {
-          //success
-          resolve(res.data);
-        },
-        function(res) {
-          //error
-          reject(res.data);
+            saveFunction: function(createFunction) {
+                return $q(function(resolve, reject) {
+                    $http.post('/compositefunction/' + createFunction.name, createFunction)
+                        .then(function(res) {
+                                //success
+                                resolve(res.data);
+                            },
+                            function(res) {
+                                //error
+                                reject(res.data);
+                            }
+                        );
+                });
+            },
+
+            getFunctionByName: function(fkn) {
+                return $q(function(resolve, reject) {
+                    console.log(fkn);
+                    $http.get('/compositefunction/' + fkn)
+                        .then(function(res) {
+                                //success
+                                resolve(res.data);
+                            },
+                            function(res) {
+                                //error
+                                reject(res.data);
+                            });
+                });
+            },
+
+            saveTest: function(fkn) {
+                return $q(function(resolve, reject) {
+                    $http.post('/compositefunction/test', fkn)
+                        .then(function(res) {
+                                //success
+                                resolve(res.data);
+                            },
+                            function(res) {
+                                //error
+                                reject(res.data);
+                            });
+                });
+            },
+
+            getFunction: function() {
+                return $q(function(resolve, reject) {
+                    $http.get('/compositefunction')
+                        .then(function(res) {
+                                //success
+                                resolve(res.data);
+                            },
+                            function(res) {
+                                //error
+                                reject(res.data);
+                            });
+                });
+            },
+
         }
-        );
-      });
-    },
 
-    getFunctionByName: function(fkn) {
-    return $q(function(resolve, reject) {
-      console.log(fkn);
-      $http.get('/compositefunction/'+fkn)
-      .then(function(res) {
-        //success
-        resolve(res.data);
-      },
-      function(res) {
-        //error
-        reject(res.data);
-      });
-    });
-  },
-
-  saveTest: function(fkn) {
-    return $q(function(resolve, reject) {
-      $http.post('/compositefunction/test',fkn)
-      .then(function(res) {
-        //success
-        resolve(res.data);
-      },
-      function(res) {
-        //error
-        reject(res.data);
-      });
-    });
-  },
-
-    getFunction: function() {
-    return $q(function(resolve, reject) {
-      $http.get('/compositefunction')
-      .then(function(res) {
-        //success
-        resolve(res.data);
-      },
-      function(res) {
-        //error
-        reject(res.data);
-      });
-    });
-  },
-
-  }
-
-  //end of factory definition
-  return factory;
-}]);
+        //end of factory definition
+        return factory;
+    }]);
