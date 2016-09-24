35a477737eedc21024c095fd273acb92570538d2
inderjeet <inderjeet.singh12@wipro.com>
2016-09-24 10:40:56 +0000

    validation in Historic Data time selection before start

--- tattvaclient/design/watchlists/controller/historicDataCtrl.js
+++ tattvaclient/design/watchlists/controller/historicDataCtrl.js
@@ -2,20 +2,6 @@ angular.module('tattva')
 .controller('historicDataCtrl',['$scope','$mdDialog','$filter',function($scope,$mdDialog,$filter){
 	$scope.focusinControl = {};
 	$scope.showHistoricData = function(){
-		if($scope.fromDate == undefined || $scope.fromTime == undefined){
-			$mdDialog.show(
-			$mdDialog.alert()
-			.parent(angular.element(document.querySelector('#popupContainer')))
-			.clickOutsideToClose(true)
-			.title('Undefined Date!')
-			.textContent('Please Select Date & Time.')
-			.ariaLabel('Select Date')
-			.ok('Ok')
-			.targetEvent()
-			);
-		}
-		else{
-			$scope.HistoricData=1;
-		} 
+		$scope.HistoricData=1;		
 	}
 }]);
